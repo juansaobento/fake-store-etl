f5e9627364a5429596790cdff182c8442c202b47
Juan Sao Bento <juansaobento@gmail.com>
2024-10-13 17:42:54 -0300

    first commit

--- fake-store-etl/dags/modules/__init__.py
+++ fake-store-etl/dags/modules/__init__.py
@@ -2,3 +2,4 @@ from .build_df import creacion_df
 from .transform import transformaciones
 from .carga_data import carga_datos
 from .utils import jdbc_properties, conection_url, creacion_spark
+import pyspark as ps
